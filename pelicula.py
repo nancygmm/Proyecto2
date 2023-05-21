74ccf4e092c6a6bd7d2fc45de39a14cb4f94dcc7
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-05-21 15:22:19 -0600

    Get y set atributos

--- pelicula.py
+++ pelicula.py
@@ -7,4 +7,27 @@ class pelicula:
         self.generos=generos
         self.saga=saga
 
-    
+    def getNombre(self):
+        return self.nombre
+    def setNombre(self,nombre):
+        self.nombre=nombre
+
+    def getDirector(self):
+        return self.director
+    def setDirector(self,director):
+        self.director=director
+
+    def getActores(self):
+        return self.actores
+    def setActores(self,actores):
+        self.actores=actores
+
+    def getGeneros(self):
+        return self.generos
+    def setGeneros(self,generos):
+        self.generos=generos
+
+    def getSaga(self):
+        return self.saga
+    def setSaga(self,saga):
+        self.saga=saga
