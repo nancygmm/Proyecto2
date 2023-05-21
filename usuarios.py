4e4c0c72a82ad262f674a5afb72381b7e5e10292
Mauricio Julio Rodrigo Lemus Guzmán <rodlemus03@gmail.com>
2023-05-21 15:17:09 -0600

    v1 usuarios

--- usuarios.py
+++ usuarios.py
@@ -1,1 +1,27 @@
+class usuarios:
+    def __init__(self,nombre,apellido,edad,cuenta):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.edad=edad
+        self.cuenta=cuenta
+        self.listaGustos=[]
+        self.playList=[]
+    # GETS
+    def getNombre(self):
+        return self.nombre
+    def getApellido(self):
+        return self.apellido
+    def getEdad(self):
+        return self.edad
+    def getCuenta(self):
+        return self.cuenta
+    def getListaGustos(self):
+        return self.listaGustos
+    def getPlayList(self):
+        return self.playList
+    # SETS
+    def setNombre(self,nombre):
+        self.nombre=nombre
+    
+
 
