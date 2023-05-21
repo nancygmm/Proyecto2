124b3d7a4d72789cae373f1c6fc42442c585b64b
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-05-21 15:15:04 -0600

    Atributos películas

--- pelicula.py
+++ pelicula.py
@@ -1,1 +1,10 @@
+class pelicula:
+    
+    def __init__(self,nombre,director,actores,generos,saga):
+        self.nombre=nombre
+        self.director=director
+        self.actores=actores
+        self.generos=generos
+        self.saga=saga
 
+    
