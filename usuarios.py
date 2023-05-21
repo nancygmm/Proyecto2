7878a16e259dda6ee82fd8356c8e60f5aad9611d
Mauricio Julio Rodrigo Lemus Guzmán <rodlemus03@gmail.com>
2023-05-21 16:37:12 -0600

    Update usuarios.py

--- usuarios.py
+++ usuarios.py
@@ -22,6 +22,19 @@ class usuarios:
     # SETS
     def setNombre(self,nombre):
         self.nombre=nombre
+    def setApellido(self,apellido):
+        self.apellido=apellido
+    def setEdad(self,edad):
+        self.edad=edad
+    def setCuenta(self,cuenta):
+        self.cuenta=cuenta
+    def SetListaGustos(self,listaGustos):
+        self.listaGustos=listaGustos
+    # METODOS 
+    def agregarPlayList(self,pelicula):
+        self.playList.append(pelicula)
+    def quitarPlayList(self,pelicula):
+        self.playList.remove(pelicula)
     
 
 
