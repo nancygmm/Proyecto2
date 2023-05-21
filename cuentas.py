9f7fef3944a39b7d2e8b608d20145c613dba819a
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2023-05-21 15:14:32 -0600

    actualizaciones cuenta

--- cuentas.py
+++ cuentas.py
@@ -1,1 +1,32 @@
+class cuentas:
+    def __init__(self, correo, contraseña, telefono, tipo):
+        self.correo = correo
+        self.contraseña = contraseña
+        self.telefono = telefono
+        self.tipo = tipo
+    
+    def getCorreo (self):
+        return self.correo
+    
+    def setCorreo (self,correo):
+        self.correo = correo
+    
+    def getContraseña (self):
+        return self.contraseña
+    
+    def setContraseña (self,contraseña):
+        self.contraseña = contraseña
+
+    def getTelefono (self):
+        return self.telefono
+    
+    def setTelefono (self,telefono):
+        self.telefono = telefono
+    
+    def getTipo (self):
+        return self.tipo
+    
+    def setTipo (self,tipo):
+        self.tipo = tipo
+    
 
