0237f78b4f8a3dd6e4016eb503b2f1343ac05007
Riv2oo4 <riv22500@uvg.edu.gt>
2023-05-21 19:24:31 -0600

    Declaración de gets y sets

--- genero.py
+++ genero.py
@@ -2,3 +2,15 @@ class Genero:
     def __init__(self, nombre, compatibilidad):
         self._nombre = nombre
         self._compatibilidad = compatibilidad
+        
+    def get_nombre(self):
+        return self._nombre
+
+    def set_nombre(self, nombre):
+        self._nombre = nombre
+
+    def get_compatibilidad(self):
+        return self._compatibilidad
+
+    def set_compatibilidad(self, compatibilidad):
+        self._compatibilidad = compatibilidad
