12c05d7cc0cff0cc650d78d80598981b5df18630
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2023-05-21 22:08:39 -0600

    actualizacion main

--- Main.py
+++ Main.py
@@ -19,15 +19,15 @@ class Main:
         opcionMenu = input("Elige una opción ")
         if opcionMenu=="1":
             print ("Por favor ingrese sus credenciales")
-            cuentaE = cuentas()
             correo = input("Por favor ingrese su correo electrónico ->   ")
             contraseña = input("Por favor ingrese su contraseña ->   ")
             telefono = input("Por favor ingrese su número de teléfono ->   ")
             tipo = input("Por favor ingrese el tipo ->   ")
+            Ncuenta=cuentas(correo, contraseña, telefono, tipo)
             
         elif opcionMenu=="2":
-            print ("")
-            input("Has pulsado la opción 2")
+            print ("Ingresa la información que se te solicita")
+            
         elif opcionMenu=="4":
             break
         else:
