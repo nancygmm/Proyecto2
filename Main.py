a3a194bdab4513e67dac41551c374be3d3ff6e16
Nancy Mazariegos <nancymazariegos03@gmail.com>
2023-05-21 21:31:28 -0600

    Menú

--- Main.py
+++ Main.py
@@ -1,34 +1,31 @@
 import os
+import usuarios
+from neo4j import GraphDatabase
 
 class Main: 
     
     def menu():
-        """
-        Función que limpia la pantalla y muestra nuevamente el menu
-        """
+        
         os.system('clear')
-        print ("Bienvenido")
-        print ("\t1 -> primera opción")
-        print ("\t2 -> segunda opción")
-        print ("\t3 -> tercera opción")
+        print ("Bienvenido a teleflix")
+        print("¿Ya tienes una cuenta?")
+        print ("\t1 -> Si :) ")
+        print ("\t2 -> No :( ")
         print ("\t4 -> salir")
-    
     
     while True:
         menu()
-
         opcionMenu = input("Elige una opción ")
         if opcionMenu=="1":
-            print ("")
+            print ("Por favor ingrese sus credenciales")
             input("opción 1")
         elif opcionMenu=="2":
             print ("")
             input("Has pulsado la opción 2")
-        elif opcionMenu=="3":
-            print ("")
-            input("Has pulsado la opción 3")
         elif opcionMenu=="4":
             break
         else:
             print ("")
             input("Opción no válida")
+    
+    
