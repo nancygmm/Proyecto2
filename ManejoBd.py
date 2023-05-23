3f8dffbb8b1256bf2e4b86670ca6b941c1aab133
Mauricio Julio Rodrigo Lemus Guzmán <rodlemus03@gmail.com>
2023-05-22 20:13:22 -0600

    manejo de base de datos con CREATE implementado

--- ManejoBd.py
+++ ManejoBd.py
@@ -9,6 +9,23 @@ class manejoBd:
         with self.driver.session() as session:
             result = session.run(query)
             return list(result)
+    def crearUsuario(self,nombre):
+        with self.driver.session() as sesion:
+            result=sesion.write_transaction(
+                lambda tx: tx.run("CREATE (n:nombre {name: $nombre}) RETURN n", nombre=nombre)
+            )
+    def crearGenero(self,genero):
+       with self.driver.session() as session:
+        result = session.write_transaction(
+            lambda tx: tx.run("CREATE (g:Genero {name: $genero}) RETURN g", genero=genero)
+        )
+    def crearPelicula(self,pelicula):
+       with self.driver.session() as session:
+        result = session.write_transaction(
+            lambda tx: tx.run("CREATE (p:Pelicula {name: $pelicula}) RETURN p", pelicula=pelicula)
+        )
+            
 
 
-app=manejoBd()
+
+
--- pruebasbd.py
+++ pruebasbd.py
@@ -3,9 +3,13 @@ import ManejoBd
 manejo=ManejoBd.manejoBd()
 
 
-query = "MATCH (n) RETURN n"
-result = manejo.execute_query(query)
-for i in result:
-    nodo=i["n"]
-    print(nodo)
-    print(nodo["name"])
+#query = "MATCH (n) RETURN n"
+#query="Gaby"
+#manejo.crearUsuario(query)
+
+genero="Titanes del pacifica "
+manejo.crearPelicula(genero)
+#for i in result:
+#    nodo=i["n"]
+#    print(nodo)
+#    print(nodo["name"])
