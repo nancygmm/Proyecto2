c31fbb5b2cecc2e5eaa094eb8a98ff0b901e2db9
Mauricio Julio Rodrigo Lemus Guzmán <rodlemus03@gmail.com>
2023-05-25 23:08:20 -0600

    pruebasMatriz

--- pruebasbd.py
+++ pruebasbd.py
@@ -3,13 +3,8 @@ import random
 manejo=ManejoBd.manejoBd()
 dicc=manejo.obtenerRelaciones()
 diccionarioGeneros1={}
-diccionarioGeneros2={}
-diccionarioGeneros3={}
-xxx=manejo.obtenerGeneros()
-for i in xxx:
-    diccionarioGeneros1[i]=0
-    diccionarioGeneros2[i]=0
-    diccionarioGeneros3[i]=0
+
+
 
 
 
@@ -34,13 +29,59 @@ print(listaMeGusta)
 
 
 
-def creacionMatriz(lista):
-    l=[]
-    for i in range(len(lista)):
-        f=[]
-        for j in range(len(xxx)):
-            f.append(0)
-        l.append(f)
-    print(l)
-matriz=creacionMatriz(listaMeGusta)
+
+
+
+def obtenerGeneroPelicula():
+    diccionarioGenero={}
+    rel=manejo.obtener_relaciones_pelicula_genero()
+    for i in rel:
+        laux=[]
+        for j in rel[i]:
+            laux.append(j['name'])
+        diccionarioGenero[i[0][1]]=laux
+    return diccionarioGenero
+peliculasGeneros=obtenerGeneroPelicula()
+
+def diccionarioInicialGeneros():
+    xxx=manejo.obtenerGeneros()
+    dic={}
+    for i in xxx:
+        dic[i]=0
+    return dic
+
+def puntaje10(diccionario,listaLiked):
+    diccionarioMeGusta={}
+    matriz=[]
+    for i in listaLiked:
+        diccionarioMeGusta[i]=diccionario[i]
+        d=diccionarioInicialGeneros()
+        for j in diccionarioMeGusta[i]:
+            d[j]=1
+        matriz.append(list(d.values()))
+    return matriz
+puntajes=[3,4,5]
+def multiplicarPuntaje(puntajes):
+    matrizAusar=puntaje10(peliculasGeneros,listaMeGusta)
+    for i in range(len(puntajes)):
+        for j in range(len(matrizAusar[0])):
+            x=matrizAusar[i][j]
+            matrizAusar[i][j]=puntajes[i]*x
+    return matrizAusar
+
+matrizValores=multiplicarPuntaje(puntajes)
+
+
+def calcularPob(matriz):
+    lista=[]
+    for i in range(len(matriz[0])):
+        suma=0
+        suma=matriz[0][i]+matriz[1][i]+matriz[2][i]
+        lista.append(round(suma/15,2))
+    return lista
+print(calcularPob(matrizValores))
+
+
+
+    
 
