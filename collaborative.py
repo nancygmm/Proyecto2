1244c206a3fb8bd72bfab4f7d53b8c87455ca688
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2023-05-25 22:51:05 -0600

    correcciones

--- collaborative.py
+++ collaborative.py
@@ -8,20 +8,9 @@ import matplotlib.pyplot as plt
 
 import ManejoBd as bd
 
-def collaborative (matriz, puntos, matrizG):
 
-    filas = matriz[:3]
 
-    filasOP= filas*puntos
 
-    Nmatriz = np.vstack(filasOP)
-
-    ptotales = np.sum(Nmatriz, axis = 0)
-
-    listaPtotales = []
-
-    for i in ptotales:
-        listaPtotales.append(i)
 
     
 
