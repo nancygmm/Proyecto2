90e93927fb891d2c66fc2af17c083da9a85afa60
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2023-05-26 22:18:03 -0600

    correciones y comentarios

--- ManejoBd.py
+++ ManejoBd.py
@@ -1,30 +1,44 @@
+#se importan las librerias a usar
+
 from neo4j import GraphDatabase
 import csv
 class manejoBd:
+
+    #constructor
     def __init__(self):
         self.driver=self.conectar()
+    
+    #funcion para conectar con la base de datos local
     def conectar(self):
         driver=GraphDatabase.driver("bolt://localhost:7687",auth=("neo4j","douglasdouglas"))
         return driver
+    
+    #funcion que recibe los querys y los corre
     def execute_query(self,query):
         with self.driver.session() as session:
             result = session.run(query)
             return list(result)
+        
+    #funcion que crea un usuario y tambien crea los nodos y relaciones necesarias en la base de datos
     def crearUsuario(self,nombre):
         with self.driver.session() as sesion:
             result=sesion.write_transaction(
                 lambda tx: tx.run("CREATE (n:nombre {name: $nombre}) RETURN n", nombre=nombre)
             )
+     #funcion que crea un generp y tambien crea los nodos y relaciones necesarias en la base de datos
     def crearGenero(self,genero):
        with self.driver.session() as session:
         result = session.write_transaction(
             lambda tx: tx.run("CREATE (g:Genero {name: $genero}) RETURN g", genero=genero)
         )
+     #funcion que crea una pelicula y tambien crea los nodos y relaciones necesarias en la base de datos
     def crearPelicula(self,pelicula):
        with self.driver.session() as session:
         result = session.write_transaction(
             lambda tx: tx.run("CREATE (p:Pelicula {name: $pelicula}) RETURN p", pelicula=pelicula)
         )
+    
+    #funcion que obtiene las relaciones de las peliculas en la clase de datos
     def obtener_relaciones_pelicula_genero(self):
         driver = self.conectar()
         
@@ -70,6 +84,7 @@ class manejoBd:
 
             return peliculas
 
+    #obtiene las relaciones de cualquier nodo
     def obtenerRelaciones(self):
        diccionario_relaciones = self.obtener_relaciones_pelicula_genero()
        diccionarioPeliculaGenero={}
@@ -83,6 +98,7 @@ class manejoBd:
        return diccionarioPeliculaGenero
     
 
+    #funcion para obtener los usuarios
     def obtenerUsuarios(self):
       driver=self.conectar()
 
